f747cc76dd9e7ca5f1fa1e971aee252b0bff1c95
Mowalky <mowalky@outlook.com>
2022-06-23 21:34:28 -0500

    first commit

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { Button, Input, Flex, Checkbox, Heading } from "@chakra-ui/react";
 
+import useStore from "../store";
+
 function TodoListItems() {
+  const store = useStore();
   return (
     <>
-      {[].map((todo: { id: number; text: string }) => (
+      {store.todos.map((todo: any) => (
         <Flex pt={2} key={todo.id}>
           <Checkbox />
           <Input mx={2} value={todo.text} />
--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,5 @@
+import create from "zustand";
+
 // Standard interface and functions
 export interface Todo {
   id: number;
@@ -5,22 +7,22 @@ export interface Todo {
   done: boolean;
 }
 
-export const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
+const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
   todos.map((todo) => ({
     ...todo,
     text: todo.id === id ? text : todo.text,
   }));
 
-export const toggleTodo = (todos: Todo[], id: number): Todo[] =>
+const toggleTodo = (todos: Todo[], id: number): Todo[] =>
   todos.map((todo) => ({
     ...todo,
     done: todo.id === id ? !todo.done : todo.done,
   }));
 
-export const removeTodo = (todos: Todo[], id: number): Todo[] =>
+const removeTodo = (todos: Todo[], id: number): Todo[] =>
   todos.filter((todo) => todo.id !== id);
 
-export const addTodo = (todos: Todo[], text: string): Todo[] => [
+const addTodo = (todos: Todo[], text: string): Todo[] => [
   ...todos,
   {
     id: Math.max(0, Math.max(...todos.map(({ id }) => id))) + 1,
@@ -28,3 +30,51 @@ export const addTodo = (todos: Todo[], text: string): Todo[] => [
     done: false,
   },
 ];
+
+type Store = {
+  todos: Todo[];
+  newTodo: string;
+  addTodo: () => void;
+  setNewTodo: (text: string) => void;
+  update: (id: number, text: string) => void;
+  toggle: (id: number) => void;
+  remove: (id: number) => void;
+};
+
+const useStore = create<Store>((set) => ({
+  todos: [],
+  newTodo: "",
+  addTodo() {
+    set((state) => ({
+      ...state,
+      todos: addTodo(state.todos, state.newTodo),
+      newTodo: "",
+    }));
+  },
+  setNewTodo(text: string) {
+    set((state) => ({
+      ...state,
+      newTodo: text,
+    }));
+  },
+  update(id: number, text: string) {
+    set((state) => ({
+      ...state,
+      todos: updateTodo(state.todos, id, text),
+    }));
+  },
+  toggle(id: number) {
+    set((state) => ({
+      ...state,
+      todos: toggleTodo(state.todos, id),
+    }));
+  },
+  remove(id: number) {
+    set((state) => ({
+      ...state,
+      todos: removeTodo(state.todos, id),
+    }));
+  },
+}));
+
+export default useStore;
